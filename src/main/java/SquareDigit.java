a1ec6cb54d80d16b78da4be60585f296826cf054
rwm774s@hotmail.com <rwm774s@hotmail.com>
2022-02-14 00:02:49 -0600

    Final passing solution

--- src/main/java/SquareDigit.java
+++ src/main/java/SquareDigit.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SquareDigit {
 
     public int squareDigits(int digits) {
+        String strDigits = String.valueOf(digits);
+        List<String> digitList = new ArrayList<>();
+        String answer = "";
 
+        //Split string into each value and add to arraylist
+        digitList = Arrays.asList(strDigits.split(""));
+
+        //Use special for loop to move through each item in list
+        for (String number : digitList) {
+
+            //Change each value to an int, use Math.pow to raise the number to second power, format no decimals, add to string
+            answer = answer + String.format("%.0f", Math.pow(Integer.parseInt(number), 2));
+        }
+
+        //Return the final string converted to an int
+        return Integer.parseInt(answer);
     }
 }
